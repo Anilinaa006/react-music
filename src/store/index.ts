d6107a01a1a6b358dadee49d99ce4cc32e491c91
anilin <2840668784@qq.com>
2026-04-07 17:49:03 +0800

    07-useDispatch的hooks封装

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import type { TypedUseSelectorHook } from 'react-redux'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
 
 const store = configureStore({
   reducer: {}
 })
 
 type GetStateFnType = typeof store.getState
-export type IRootState = ReturnType<GetStateFnType>
+type IRootState = ReturnType<GetStateFnType>
+type DispatchType = typeof store.dispatch
 
 // 定义一个自定义的 useSelector 函数，用于在组件中使用
 export const useAppSelector: TypedUseSelectorHook<IRootState> = useSelector
+
+// 定义一个自定义的 useDispatch 函数，用于在组件中使用
+export const useAppDispatch: () => DispatchType = useDispatch
+
+export const shallowEqualApp = shallowEqual
 
 export default store
